af9bee81207d3967a5631b292040ac8006ebc903
jfox <atljeremy@me.com>
2014-01-10 20:24:24 -0500

    Setting up gh-pages

--- PetFinderAPI/Private/PFMacros.h
+++
@@ -1,9 +0,0 @@
-//
-//  PFMacros.h
-//  PetFinderAPI
-//
-//  Created by Jeremy Fox on 1/7/14.
-//  Copyright (c) 2014 Jeremy Fox. All rights reserved.
-//
-
-#define VALUE_OR_DEFAULT(VALUE, DEFAULT) (VALUE && ![VALUE isKindOfClass:[NSNull class]]) ? VALUE : DEFAULT
